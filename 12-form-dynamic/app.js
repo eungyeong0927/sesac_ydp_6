1bb141afb4bc01ebd213f92feb4935800067e1d9
eungyeong0927 <bb060@naver.com>
2024-06-17 16:16:05 +0900

    study : http 비동기 통신 - ajax

---
+++ 12-form-dynamic/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// app.post("/", (req, res) => {
+//     res.render("dynamic", { title: "프로젝트 만드는 걸 연습하자" });
+// });
+
+app.get("/", (req, res) => {
+    res.render("dynamic", { title: "동적 폼을 배워보자" });
+});
+
+// ajax
+app.get("/ajax", (req, res) => {
+    // GET 방식이므로 브라우저에서 URL 쿼리까지 직접 입력해서도 값을 확인 가능
+    // => ex. http://localhost:8000/ajax?name=은경&gender=여자
+    console.log(req.query);
+    res.send(req.query);
+});
+
+app.post("/ajax", (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
