6fe94b9ec7171a51d6a44ae35b911d20c1444fa7
eungyeong0927 <bb060@naver.com>
2024-06-26 18:37:33 +0900

    study: mvc 이용해서 방명록 추가/수정/삭제 기능(mySQL연동까지) 만들기

---
+++ 16-mvc_mysql/controller/CVisitor.js
@@ -0,0 +1,63 @@
+const Visitor = require("../model/Visitor");
+
+// (1) GET / => localhost:PORT/
+exports.main = (req, res) => {
+    res.render("index");
+};
+
+// (2) GET /visitors => localhost:PORT/visitors
+exports.getVisitors = (req, res) => {
+    // console.log(Visitor.getVisitors); // [Function (anonymous)]
+
+    // [before]
+    // res.render('visitor', { data: Visitor.getVisitors() });
+
+    // [after]
+    Visitor.getVisitors((result) => {
+        // result 매개변수 : model/Visitor.js getVisitors함수의 callback(rows)의 "rows" 변수에 대응
+
+        console.log("controller/CVisitor.js >> ", result);
+
+        res.render("visitor", { data: result });
+    });
+};
+
+exports.postVisitor = (req, res) => {
+    console.log(req.body);
+
+    Visitor.postVisitor(req.body, (result) => {
+        // req.body : static> visitor.js 에서 axios 형식으로 data를 보내고 있는 걸 받음
+        // result : rows.insertId
+        console.log("controller/CVisitor.js >> ", result);
+        // controller/CVisitor.js >>  4
+
+        res.send({ id: result, name: req.body.name, comment: req.body.comment });
+    });
+};
+
+exports.getVisitor = (req, res) => {
+    // req.params.id: // 조회해야할 id
+    Visitor.getVisitor(req.params.id, (result) => {
+        res.send(result);
+    });
+};
+
+exports.deleteVisitor = (req, res) => {
+    console.log(req.body);
+
+    Visitor.deleteVisitor(req.body.id, (result) => {
+        console.log("controller/CVisitor.js >> ", result);
+
+        res.send({ result }); // { result: result }
+    });
+};
+
+exports.patchVisitor = (req, res) => {
+    console.log(req.body);
+
+    Visitor.patchVisitor(req.body, (result) => {
+        console.log("controller/CVisitor.js >> ", result);
+
+        res.send({ result }); // { result: result }
+    });
+};
