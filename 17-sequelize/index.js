cba843c78ef9ce6b07216f3f34d4d205bd51270e
eungyeong0927 <bb060@naver.com>
2024-06-28 17:47:52 +0900

    study : add api about player with sequelize

--- 17-sequelize/index.js
+++ 17-sequelize/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
-const PORT = 8300;
+const PORT = 8000;
 const router = require("./routes/index");
+const playerRouter = require("./routes/player");
 const { sequelize } = require("./models/index");
 
 app.set("view engine", "ejs");
@@ -10,11 +11,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/", router);
+app.use("/players", playerRouter);
 
 sequelize
-    // force : true; 서버 실행 때마다 테이블을 재생성
-    // force : false; 서버 실행 시 테이블이 없으면 생성
-    .sync({ force: true })
+    // force: true; 서버 실행때 마다 테이블을 재 생성
+    // force: false; 서버 실행 시 테이블이 없으면 생성
+    .sync({ force: false })
     .then(() => {
         app.listen(PORT, () => {
             console.log("Database connection succeeded!");
