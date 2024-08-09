9fc14121402c5fb7ff93246aa7355f12298c098e
eungyeong0927 <bb060@naver.com>
2024-08-09 15:52:01 +0900

    practice : lifeCycle 연습

---
+++ practice/28-lifecycle-practice/src/LifeCyclePrac.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+
+export default function LifeCyclePrac() {
+    const [post, setPosts] = useState([]);
+
+    useEffect(() => {
+        setTimeout;
+    });
+
+    return <div>LifeCyclePrac</div>;
+}
